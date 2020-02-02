4823b3eec24ec24a2b93a383fdd4ba309b3ece34
grafixeyehero <grafixeyehero@yahoo.com>
2020-02-02 18:31:04 +0300

    Fix text! module path

--- src/components/guide/guide.js
+++ src/components/guide/guide.js
@@ -1146,7 +1146,7 @@ define(['require', 'inputManager', 'browser', 'globalize', 'connectionManager', 
             }
         }
 
-        require(['text!./tvguide.template.html'], function (template) {
+        require(['text!components/guide/tvguide.template.html'], function (template) {
 
             var context = options.element;
 
